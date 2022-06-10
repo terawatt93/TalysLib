bbe362174164e4f805079d3694d0b1d9e7b459e6
Ilya <di261297@rambler.ru>
2022-06-10 17:06:31 +0300

    Обновление установки
    Обновлены bash скрипты для установки, добавлены комментарии к содержимому.
    
    В Install.sh добавились только комментарии, работоспосбность не проверялась.  После исполнения Install_Without_TALYS.sh TalysLib работает.
    
    На очереди работа с переменными окружения, чтобы постоянно не добавлялись новые строки в .bshrc при переустановке TalysLib.

--- Install.sh
+++ Install.sh
@@ -1,50 +1,45 @@
 #!/bin/bash
+#######################################################
+# This script takes care of the TalysLib installation.
+#######################################################
+# 1. Install libzip package from apt package manager
 sudo apt install -y libzip-dev
-talyslibdir=`pwd`
+talyslibdir=`pwd` # Define the TalysLib directory
 cd ..
-wget https://tendl.web.psi.ch/tendl_2019/talys/talys.tar #download archive with talys
+#
+# 2. Download and unpack archive with TALYS
+wget https://tendl.web.psi.ch/tendl_2019/talys/talys.tar 
 tar -xzf talys.tar
 cd talys
-unalias -a #The unalias utility shall remove the definition for each alias name specified. See Alias Substitution . The aliases shall be removed from the current shell execution environment; see Shell Execution Environment
+unalias -a # Note: The unalias utility shall remove the definition 
+#           for each alias name specified
 #
-# This script takes care of the TALYS installation.
-# All you have to do is adapt the next variables to your own situation,
-# and run.....
-#
+# 3. Define Fortran compiler for TALYS
 compiler='gfortran'       
 #compiler='lf95 --staticlink' 
 Thome=${HOME}
-#
-# From here on, the rest should work automatically
-#
-# 1. Define the talys directory
-#
 cd ..
-talysdir=`pwd`
+talysdir=`pwd` # Define the TALYS directory
 #
-# 2. Ensure that all directories have execute permission and that 
+# 4. Ensure that all directories have execute permission and that 
 #    all files have a read and write permission   
-#
 chmod -R u+rwX talys
 #
-# 3. Ensure that TALYS can read the structure database by replacing
+# 5. Ensure that TALYS can read the structure database by replacing
 #    the path name in subroutine machine.f
-#
 datapath=${talysdir}'/'
 datapathnew=`echo $datapath | sed 's/\//\\\\\//g'`
 cd ${talysdir}'/talys/source/'
 sed "s/ home='.*'/ home='${datapathnew}'/" machine.f  > machine_tmp.f
 mv -f machine_tmp.f machine.f
 # 
-# 4. Compile TALYS.
+# 6. Compile TALYS
 #    Please note that the compilation of ecis06t.f
 #    may result in some trivial warning messages
-#
 ${compiler} -c *.f
 ${compiler} *.o -o talys
 #
-# 5. Check whether the setup procedure has been successful
-#
+# 7. Check whether TALYS setup procedure has been successful
 if [ -e talys ] 
 then
   mv -f talys ../talys
@@ -56,16 +51,19 @@ then
   echo ' '
   echo 'You are all set to run the sample problems in the samples directory'
   echo 'with the verify script.'
-  echo  'export TALYSDIR='`pwd`>>~/.bashrc 
+  echo 'export TALYSDIR='`pwd`>>~/.bashrc 
   echo 'export PATH=$PATH:'`pwd`>>~/.bashrc
-  mkdir CalculationResults
+  mkdir CalculationResults # Create directory for TalysLib calculation results
   cd ..
-  rm talys.tar
+  rm talys.tar # Delete used TALYS archive
 else
   echo 'TALYS setup failed'
 fi
-
+#
+# 8. Return to your TalysLib directory
 cd ${talyslibdir}
+#
+# 9. Downloading and installing libxlsxwriter library
 ./MakeLibraries.sh
 make install
 
