cfca363f296ff4172e7047db30fe8c32cc22d222
terawatt93 <50338943+terawatt93@users.noreply.github.com>
2022-09-15 00:05:30 +0300

    Fixed tutorials tutorials/tutorials_for_data_analysis/GetExperimentalData

--- tutorials/tutorials_for_data_analysis/GetExperimentalData/GetAngularDistribution.cpp
+++ tutorials/tutorials_for_data_analysis/GetExperimentalData/GetAngularDistribution.cpp
@@ -8,11 +8,11 @@ void GetAngularDistribution()
     c.Print("ADIST.pdf[","pdf");
     for(unsigned int i=0;i<4;i++)
     {
-		TMultiGraph* mg=C.Products[0].Levels[i].GetEXFORTMultiGraphForAngularDistributions(14,14.5);
-		TGraph *g_talys_total=C.Products[0].Levels[i].GetAngularDistribution("Total");
-		TGraph *g_talys_dir=C.Products[0].Levels[i].GetAngularDistribution("Direct");
-		TGraph *g_talys_com=C.Products[0].Levels[i].GetAngularDistribution("Compound");
-		TGraph *g_endf=C.Products[0].Levels[i].GetAngularDistribution("ENDF");
+		TMultiGraph* mg=C.FindProductByReaction("n,n'")->Levels[i].GetEXFORTMultiGraphForAngularDistributions(14,14.5);
+		TGraph *g_talys_total=C.FindProductByReaction("n,n'")->Levels[i].GetAngularDistribution("Total");
+		TGraph *g_talys_dir=C.FindProductByReaction("n,n'")->Levels[i].GetAngularDistribution("Direct");
+		TGraph *g_talys_com=C.FindProductByReaction("n,n'")->Levels[i].GetAngularDistribution("Compound");
+		TGraph *g_endf=C.FindProductByReaction("n,n'")->Levels[i].GetAngularDistribution("ENDF");
 		mg->Add(g_talys_total,"l");
 		mg->Add(g_talys_dir,"l");
 		mg->Add(g_talys_com,"l");
