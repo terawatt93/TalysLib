a0da1e0bf818e4c5e08a5c4a5b8800d07c21f044
terawatt93 <50338943+terawatt93@users.noreply.github.com>
2023-02-14 09:30:08 +0300

    Added tutorial for all ENDF data retrieve

---
+++ tutorials/tutorials_for_data_analysis/GetEvaluatedData/GetEvaluatedAngularDistribution.cpp
@@ -0,0 +1,26 @@
+#include "TalysLib.hh"
+#include "TTeXDump.h"
+
+void GetEvaluatedAngularDistribution()
+{
+    Nucleus C("12C");
+    C.GenerateProducts();
+    TalysLibManager::Instance().SetAOption("A>=0");
+    TCanvas c;
+    c.Print("ADIST.pdf[","pdf");
+    ofstream ofs("references.txt");
+    ofs<<"Level#\tData#\tSubEntry\tAuthor\tTitle\tYear\treference\n";
+    for(unsigned int i=0;i<4;i++)
+    {
+		Level *l=&(C.FindProductByReaction("(n,n')")->Levels[i]);
+		TMultiGraph* mg=l->GetTMultiGraphForAngularDistributions("all evaluated");
+		mg->SetTitle(TString::Format("Evaluated #frac{d#sigma}{d#Omega} for level %d;#theta, deg; #frac{d#sigma}{d#Omega}, mb/str",i));
+		mg->Draw("a plc pmc ");
+		gPad->BuildLegend();
+		gPad->SetLogy(1);
+		c.Print(TString::Format("%d.tex",i));
+		c.Print("ADIST.pdf","pdf");
+		c.Clear();
+	}
+    c.Print("ADIST.pdf]","pdf");
+}
